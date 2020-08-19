2bcff439532308869b2998a9164d80db2812f3cc
jat <chencj30188@hsyuntai.com>
2020-08-19 22:18:29 +0800

    初始化

---
+++ zipkin-service/src/main/java/com/jat/zipkinservice/ZipkinServiceApplication.java
@@ -0,0 +1,14 @@
+package com.jat.zipkinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ZipkinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServiceApplication.class, args);
+    }
+
+}
